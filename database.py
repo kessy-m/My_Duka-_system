6e29040f187a2af462be5c3b1c453f0a8a1959be
kessy-m <kessymuriithi@gmail.com>
2024-05-22 17:36:25 +0300

    update2

--- database.py
+++ database.py
@@ -8,13 +8,12 @@ port= '5432'
 
 # connecting to the postgreSQL database
 conn=psycopg2.connect(
-    dbname= 'myduka',
-    user= 'postgres',
-    password= '12345',
-    host= 'localhost',
-    port= '5432'
+host= 'localhost',
+database='myduka',
+user= 'postgres',
+password= '12345',
+port= 5432
 )
-
 # open a cursor to perform database operation
 
 cur = conn.cursor()
@@ -43,8 +42,7 @@ def get_data(table_name):
 # insert values
 
 def insert_products(values):
-    cur.execute("insert into products(name,buying_price,selling_price,stock_quantity)\
-                values(%s,%s,%s,%s)",values)
+    cur.execute("insert into products(name,buying_price,selling_price,stock_quantity)values(%s,%s,%s,%s)",values)
     conn.commit()
 
 product_value=('boots', 3000, 5000, 10)
@@ -58,10 +56,61 @@ def insert_sales(values):
     conn.commit()
 
 
+# create a function to display profit per product ()
+def total_profit():
+    myprofit='select p.name, sum(((selling_price-buying_price) *quantity)) as profit from products as p join sales as s on s.pid=p.id group by p.name ;'
+    cur.execute(myprofit)
+    data=cur.fetchall()
+    return data
 
 
 
+#CREATE A FUNCTION TO DISPLAY PROFIT PER DAY
+def day_profit():
+    d_profit='select date(created_at) as day, sum((selling_price-buying_price)*quantity) as profit from products as p join sales as s on s.pid=p.id group by day order by day;'
+    cur.execute(d_profit)
+    data=cur.fetchall()
+    return data
+
+# create a function to display sales per product
+def total_sales():
+    t_sales="select p.name ,sum(selling_price*quantity) as result from products as p join sales as s on s.pid=p.id group by p.name;"
+    cur.execute(t_sales)
+    data=cur.fetchall()
+    return data
 
 
+# create a function to display sales per day
+def day_sales():
+    d_sales='select date(created_at) as day, sum((selling_price*quantity))as profit from products as p join sales as s on s.pid=p.id group by day;'
+    cur.execute(d_sales)
+    data=cur.fetchall()
+    return (data)
 
 
+# insert user
+def insert_user(values):
+   query='insert INTO users (full_name, email, password) values(%s,%s,%s);'
+   cur.execute(query,values)
+   conn.commit()
+
+# check email
+def check_email(email):
+       query='select * from users where email=%s '
+       cur.execute(query,(email,))
+       data=cur.fetchone()
+       return data
+
+#  check credentials
+def check_email_exist(email):
+    query='select * from users where email=%s'
+    cur.execute(query,(email,))
+    data=cur.fetchall()
+    return data
+
+#check if email and password exists
+def check_email_pass(email,password):
+    query='select * from users where email=%s and password=%s'
+    cur.execute(query,(email,password,))
+    data=cur.fetchall()
+    return data
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from flask import Flask,render_template,request,redirect,url_for,flash
-from database import get_data,insert_products,insert_sales
+from flask import Flask,render_template,request,redirect,url_for,flash,session
+from database import get_data,insert_products,insert_sales,total_profit,day_profit,total_sales,day_sales,insert_user,check_email,check_email_exist,check_email_pass
 # flask instance
 
 app=Flask(__name__)
 app.secret_key='cupcake'
+
+
+# check login
+def check_login():
+    if 'email' not in session:
+        return redirect(url_for('login'))
+    
+
 
 @app.route('/')
 def hello():
@@ -21,6 +29,8 @@ def hello():
 # create another for products render a products.html
 @app.route('/products')
 def my_products():
+    if 'email' not in session:
+        return redirect(url_for('login'))
     products=get_data('products')
     print(products)
     return render_template('products.html',prods=products)
@@ -30,6 +40,8 @@ def my_products():
 # CREATE ONE FOR SALES
 @app.route('/sales')
 def sales():
+    if 'email' not in session:
+        return redirect(url_for('login'))
     sales=get_data('sales')
     prods=get_data('products')
     print(sales)
@@ -39,7 +51,43 @@ def sales():
 # CREATE ONE FOR DASHBOARD
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    if 'email' not in session:
+        return redirect(url_for('login'))
+    profit=total_profit()
+    name_profit=[]
+    value_profit=[]
+    for i in profit:
+        name_profit.append(str(i[0]))
+        value_profit.append(float(i[1]))
+        
+        
+# create profit per day
+    dayprofit=day_profit()
+    days=[]
+    d_profit=[]
+    for d in dayprofit:
+        days.append(str(d[0]))
+        d_profit.append(float(d[1]))
+
+# create sales per product
+    mysales=total_sales()
+    sales=[]
+    sales_products=[]
+    for s in mysales:
+        sales.append(str(s[0]))
+        sales_products.append(float(s[1]))
+
+ # create sales per day
+    allsales=day_sales()
+    s_day=[]   
+    t_sales=[]   
+    for w in allsales:
+        s_day.append(str(w[0]))  
+        t_sales.append(float(w[1]))
+    return render_template('dashboard.html',profit=value_profit,name=name_profit,days=days,p_d=d_profit,sales1=sales,s_p=sales_products,daysales=s_day,totalsales=t_sales)
+
+
+
 
 # insert products
 @app.route('/add_products',methods=['POST','GET'])
@@ -69,4 +117,60 @@ def make_sales():
     insert_sales(mysales)
     return redirect(url_for('sales'))
 
-app.run(debug=True)
+
+
+# create a login route
+@app.route('/login', methods=['POST','GET'] )
+def login():
+    # get form data
+    if request.method=='POST':
+        email=request.form['email']
+        password=request.form['password']
+
+        #  check email exists
+        ch_email=check_email_exist(email)
+        if len(ch_email)<1:
+            flash('email does not exist')
+            return redirect(url_for('register'))
+        else:
+            ch_pass=check_email_pass(email,password)
+            if len(ch_pass)<1:
+                flash('try again')
+            else:
+                session['email']=email
+                flash('login succesfully')    
+                return redirect(url_for('dashboard'))
+    return render_template('login.html')
+
+# create a log out route
+@app.route('/log_out')
+def log_out():
+    session.pop('email',None)
+    flash('logout successfully',)
+    return redirect(url_for('login'))
+
+
+
+# create a register route 
+@app.route('/register', methods=['POST','GET'])
+def register():
+    if request.method=="POST":
+        f_name=request.form['full_name']
+        email=request.form['email']
+        password=request.form['password']
+# insert user
+        ch_email=check_email(email)
+        if len(ch_email)<1:
+            new_user=(f_name,email,password)
+            insert_user(new_user)
+            flash('register')
+            return redirect(url_for('login'))
+        else:
+            flash('email exist')
+    return render_template('register.html')
+
+
+
+# app.run(debug=True)
+
+
